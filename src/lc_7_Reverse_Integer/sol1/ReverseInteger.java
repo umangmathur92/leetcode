5738a5c34908d62efcfd661eb4faf3f7c10ab12d
Umang Mathur <umangmathur10051992@gmail.com>
2017-11-16 19:07:28 -0800

    reverse integer

---
+++ src/lc_7_Reverse_Integer/sol1/ReverseInteger.java
@@ -0,0 +1,33 @@
+package lc_7_Reverse_Integer.sol1;
+
+import Utils.Util;
+
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        int n = 946324351;
+        int numOfIntervals = reverse(n);
+        Util.print("Reversed integer : " + numOfIntervals);
+    }
+
+    public static int reverse(int n) {
+        boolean hasMinusSign = false;
+        String str = String.valueOf(n);
+        if (str.startsWith("-")) {
+            str = str.replace("-","");
+            hasMinusSign = true;
+        }
+        int strLen = str.length();
+        String outPutStr = "";
+        for (int i=strLen-1; i >= 0; i--) {
+            outPutStr = outPutStr + str.substring(i, i+1);
+        }
+        outPutStr = hasMinusSign ? "-" + outPutStr : outPutStr;
+        try {
+            return Integer.parseInt(outPutStr);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+}
---
+++ src/lc_7_Reverse_Integer/sol2/ReverseInteger.java
@@ -0,0 +1,25 @@
+package lc_7_Reverse_Integer.sol2;
+
+import Utils.Util;
+
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        int n = 7295;
+        int numOfIntervals = reverse(n);
+        Util.print("Reversed integer : " + numOfIntervals);
+    }
+
+    public static int reverse(int n) {
+        long revNum = 0;
+        while (n !=0) {
+            revNum = revNum * 10 + n%10;
+            n = n/10;
+            if(revNum > Integer.MAX_VALUE || revNum < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) revNum;
+    }
+
+}
